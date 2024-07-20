ae3443adfad7141b33d30655db700de57867d758
jvhaile <jvhaileapps@gmail.com>
2024-07-20 15:16:12 +0300

    changed the env creation variables to return the record directly

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@ import {NodeProcessEnvSource} from "./env-source/sources/node-process-env-source
 import {MemoryEnvSource} from "./env-source/sources/memory-env-source";
 import {DotEnvFileSource} from "./env-source/sources/dot-env-file-source";
 
-export function nodeProcessEnv<T extends EnvRecord>(schema: T): Env<inferEnvType<T>> {
-    return Env.create(new NodeProcessEnvSource(), schema)
+export function nodeProcessEnv<T extends EnvRecord>(schema: T): inferEnvType<T> {
+    return Env.create(new NodeProcessEnvSource(), schema).getRecord()
 }
 
-export function memoryEnv<T extends EnvRecord>(schema: T, record: Record<string, any>): Env<inferEnvType<T>> {
-    return Env.create(new MemoryEnvSource(record), schema)
+export function memoryEnv<T extends EnvRecord>(schema: T, record: Record<string, any>): inferEnvType<T> {
+    return Env.create(new MemoryEnvSource(record), schema).getRecord()
 }
 
-export function dotEnv<T extends EnvRecord>(path: string, schema: T): Env<inferEnvType<T>> {
-    return Env.create(new DotEnvFileSource(path), schema)
+export function dotEnv<T extends EnvRecord>(path: string, schema: T): inferEnvType<T> {
+    return Env.create(new DotEnvFileSource(path), schema).getRecord()
 }
 
 
-export function createEnv<T extends EnvRecord>(source: SourceOption,schema: T): Env<inferEnvType<T>> {
+
+export function createEnv<T extends EnvRecord>(source: SourceOption, schema: T): inferEnvType<T> {
     switch (source.source) {
         case "memory":
             return memoryEnv(schema, source.record)
@@ -25,7 +26,7 @@ export function createEnv<T extends EnvRecord>(source: SourceOption,schema: T): 
         case "dotenv":
             return dotEnv(source.path || '.env', schema)
         case "custom":
-            return Env.create({fetch: source.fetch}, schema)
+            return Env.create({fetch: source.fetch}, schema).getRecord()
     }
 }
 
